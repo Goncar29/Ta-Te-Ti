a14a9694ce8a67f7b925a308ccce73ef4b8a7c28
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-04 16:21:11 -0300

    Se crea tablero de juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import './App.css'
+
+
+const TURNS ={
+    X: 'X',
+    O: 'O'
+}
+
+console.log(TURNS)
+
+const Square = ({ children, updateBoard, index }) => {
+    return (
+        <div className="square">
+            {children}
+        </div>
+    )
+}
 
 function App() {
 
+    const [board, setBoard] = useState(Array(9).fill(null))
+    console.log(board)
+    console.log(setBoard)
     return (
-        <>
+        <main className="board">
             <h1>Ta Te Ti</h1>
-        </>
+            <section className="game">
+                {
+                    board.map((index) => {
+                        return (
+                            <Square 
+                                index={index}
+                                key={index}
+                            >
+                                <span className="cell__content">
+                                    {index}
+                                </span>
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+        </main>
     )
 }
 
