29a968f89acd881ca9f6c513ac0714b216c3282f
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-06 16:45:40 -0300

    Guardamos las partidas usando localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,15 @@ import { Tablero } from './components/Tablero.jsx'
 import './App.css'
 
 function App() {
-    const [board, setBoard] = useState(Array(9).fill(null))
-    const [turn, setTurn] = useState(TURNS.X)
+    const [board, setBoard] = useState(() => {
+        const boardFromStorage = window.localStorage.getItem('board')
+        if (boardFromStorage) return JSON.parse(boardFromStorage)
+        return Array(9).fill(null)
+    })
+    const [turn, setTurn] = useState(() => {
+        const turnFromStorage = window.localStorage.getItem('turn')
+        return turnFromStorage ?? TURNS.X
+    })
     // null es que no hay ganador, false es que hay un empate
     const [winner, setWinner] = useState(null)
 
@@ -17,6 +24,9 @@ function App() {
         setBoard(Array(9).fill(null))
         setTurn(TURNS.X)
         setWinner(null)
+
+        window.localStorage.removeItem('board')
+        window.localStorage.removeItem('turn')
     }
 
 
@@ -34,6 +44,9 @@ function App() {
         //cada vez que hacemos click en un cuadro cambiamos el turno
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
         setTurn(newTurn)
+
+        // guardar la partida
+        window.localStorage.setItem('board', JSON.stringify(newBoard))
         // revisar si hay ganador
         const newWinner = checkWinnerFrom(newBoard)
         if (newWinner) {
